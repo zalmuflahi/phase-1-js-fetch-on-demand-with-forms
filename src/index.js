bed9e1a178bc7094abeb8a68461d7c6d40f14e9c
Zaid Almuflahi <zaid.almuflahi1217@gmail.com>
2022-10-28 21:10:18 -0400

    commit tree 490a95f6c77b9b80390f38a50d6f40c3f351432f

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form');
+
+  inputForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const input = document.querySelector('input#searchByID');
+   
+    console.log(input.value); 
+
+    return fetch('http://localhost:3000/movies')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+  });
+});
 }
 
+
+
 document.addEventListener('DOMContentLoaded', init);
+
